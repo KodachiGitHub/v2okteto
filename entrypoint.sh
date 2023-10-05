60f396e67294638984d806e36d1357fe092197cf
Kodachi <jk574439826@gmail.com>
2023-10-06 02:16:57 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,8 @@ DIR_CONFIG="/etc/v2ray"
 DIR_RUNTIME="/usr/bin"
 DIR_TMP="temp_dir"
 
+mkdir -p temp_dir
+
 PORT=2048
 PASSWORD=password
 
@@ -27,7 +29,6 @@ cat << EOF > temp_dir/heroku.json
 EOF
 
 # Get V2Ray executable release
-mkdir -p temp_dir
 curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o temp_dir/v2ray_dist.zip
 busybox unzip ${DIR_TMP}/v2ray_dist.zip -d ${DIR_TMP}
 
