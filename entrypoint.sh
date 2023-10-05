c7a52ec521ff8d98f98a729cdd709ed0900edad9
Kodachi <jk574439826@gmail.com>
2023-10-06 02:29:25 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,8 +10,16 @@ mkdir -p temp_dir
 PORT=2048
 PASSWORD=password
 
+
+
+# Get V2Ray executable release
+curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o temp_dir/v2ray_dist.zip
+busybox unzip temp_dir/v2ray_dist.zip -d temp_dir
+
+# Convert to protobuf format configuration
+mkdir -p config
 # Write V2Ray configuration
-cat << EOF > temp_dir/heroku.json
+cat << EOF > config/config.json
 {
     "inbounds": [{
             "port": ${PORT},
@@ -28,17 +36,9 @@ cat << EOF > temp_dir/heroku.json
 }
 EOF
 
-# Get V2Ray executable release
-curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o temp_dir/v2ray_dist.zip
-busybox unzip temp_dir/v2ray_dist.zip -d temp_dir
-
-# Convert to protobuf format configuration
-mkdir -p config
-temp_dir/v2ctl config temp_dir/heroku.json > config/config.pb
-
 # Install V2Ray
 install -m 755 temp_dir/v2ray ${DIR_RUNTIME}
 rm -rf temp_dir
 
 # Run V2Ray
-${DIR_RUNTIME}/v2ray -config=config/config.pb
+${DIR_RUNTIME}/v2ray -config=config/config.json
